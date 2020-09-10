ca667fccf5aeb20144d763a6becec5a7c0864f99
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-09 23:04:15 -0700

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand} from 'reactstrap';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import Directory from './components/DirectoryComponent';
 
 class App extends Component {
   render() {
-      return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
-      );
+        </Navbar>
+        <Directory />
+      </div>
+    );
   }
 }
 
