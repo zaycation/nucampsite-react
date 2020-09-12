29922e9908877ad4e83ef14672aad9cc88594aaf
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-12 12:57:06 -0700

    added comments section

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
-  CardTitle,
-} from "reactstrap";
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardTitle,
+  } from "reactstrap";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Directory extends Component {
   constructor(props) {
@@ -18,21 +17,6 @@ class Directory extends Component {
 
   onCampsiteSelect(campsite) {
     this.setState({ selectedCampsite: campsite });
-  }
-
-  renderSelectedCampsite(campsite) {
-    if (campsite) {
-      return (
-        <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-          <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    }
-    return <div />;
   }
 
   render() {
@@ -52,11 +36,7 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <div className="row">
-          <div className="col-md-5 m-1">
-            {this.renderSelectedCampsite(this.state.selectedCampsite)}
-          </div>
-        </div>
+        <CampsiteInfo campsite={this.state.selectedCampsite} />
       </div>
     );
   }
