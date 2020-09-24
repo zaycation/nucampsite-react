7cacd655bbd6af567812f7bd4b529b28226bc034
zaycation.dev <isaiahthomas098@gmail.com>
2020-09-23 17:07:49 -0700

    edited ContactComponent.js

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -61,23 +61,21 @@ class Contact extends Component {
 
     const reg = /^\d+$/;
     if (this.state.touched.phoneNum && !reg.test(phoneNum)) {
-        errors.phoneNum = 'The phone number should contain only numbers.';
+      errors.phoneNum = "The phone number should contain only numbers.";
     }
 
-    if (this.state.touched.email && !email.includes('@')) {
-        errors.email = 'Email should contain a @';
+    if (this.state.touched.email && !email.includes("@")) {
+      errors.email = "Email should contain a @";
     }
 
     return errors;
-}
+  }
 
-
-handleBlur = (field) => () => {
-  this.setState({
-      touched: {...this.state.touched, [field]: true}
-  });
-}
-
+  handleBlur = (field) => () => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+    });
+  };
 
   handleInputChange(event) {
     const target = event.target;
